c742028f54e26a04f4388e17c1d326bf264bf5a2
onesoftwareengineer <48726358+onesoftwareengineer@users.noreply.github.com>
2019-06-04 16:54:39 +0300

    stilizare

--- scripts.js
+++ scripts.js
@@ -16,25 +16,38 @@ film.load();
 
 //functia care ruleaza timerul
 function StartTimer() {    
-    //schimba textul butonului deoarece a inceput timerul
+    //schimba textul butonului in ia o pauza deoarece a inceput timerul
     buton.innerText = 'Ia o pauza';
     loop = setInterval( () => {
         //verifica daca s-a terminat timpul
         if( timer > 0 ) {
             timer = timer - 1;
-            main.innerHTML = `<h1>${timer}</h1>`;            
-            console.log(timer);
+            let sirCeas = transformaInCeas(timer);
+            header.innerHTML = `<h1>${sirCeas}</h1>`;
+            document.title = `${sirCeas} min`;
+            //console.log(timer);
         }
         //daca a ajuns la zero timerul, adica pomodoro-ul a ajuns la final
         else StartReward();
     },1000);
 };
 
+//transforma un numar de secunde intr-un string de tipul 03:35 unde 03 este numarul de minute si 35 este numarul de secunde
+function transformaInCeas (x) {
+    let minuteRamase = parseInt(x/60);
+    let minuteRamaseZeci = parseInt(minuteRamase/10);
+    let minuteRamaseUnitati = minuteRamase - minuteRamaseZeci*10;
+    let secundeRamase = x - minuteRamase*60;
+    let secundeRamaseZeci = parseInt(secundeRamase/10); 
+    let secundeRamaseUnitati = secundeRamase - secundeRamaseZeci*10;
+    return `${minuteRamaseZeci}${minuteRamaseUnitati}:${secundeRamaseZeci}${secundeRamaseUnitati}`;
+};
+
 function StartReward () {
     clearTimeout(loop);
-    main.innerHTML = '';
+    header.innerHTML = '';
     //da drumul la fanfara, schimba timerul cu audio-ul cu stop the cavalry
-    main.appendChild(film);
+    header.appendChild(film);
     film.play();
 };
 
@@ -47,12 +60,18 @@ function StopTimer() {
     //rests time
     timer = pomodoroTime;
     //refreshes timer displayed
-    main.innerHTML = `<h1>${timer}</h1>`;
+    header.innerHTML = `<h1>60:00</h1>`;
     //rests button text
     buton.innerText = 'Start Pomodoro';
 };
 
 buton.addEventListener('click', () => {
-    if(buton.innerText === 'Start Pomodoro') StartTimer();
-    else if (buton.innerText === 'Ia o pauza') StopTimer();
+    if(buton.innerText === 'Start Pomodoro') {
+        StartTimer();
+        buton.classList.add('buton-pauza');
+    }
+    else if (buton.innerText === 'Ia o pauza') {
+        StopTimer();
+        buton.classList.remove('buton-pauza');
+    }
 });
